143226cc30caf8be78f98379158e297ea1393789
Otavio Campos <70704777+ocamposbio@users.noreply.github.com>
2023-08-02 14:47:40 -0300

    Add files via upload

---
+++ otavio/Spotify/Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Student21 on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
