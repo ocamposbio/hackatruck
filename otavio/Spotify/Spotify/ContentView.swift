23b19c05d40453b46c179927b7356ac5302d03d2
Otavio Campos <70704777+ocamposbio@users.noreply.github.com>
2023-08-02 15:15:59 -0300

    Add files via upload

---
+++ otavio/Spotify/Spotify/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Spotify
+//
+//  Created by Student21 on 01/08/23.
+//
+
+
+/**
+ 
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let songs =  [
+        Song (id: 1, name: "test1", artist: "test", capa: "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2019/03/c96c7a2-thank-u-next-ariana-album.jpg"),
+        Song (id: 2, name: "test2", artist: "test", capa: "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2019/03/c96c7a2-thank-u-next-ariana-album.jpg"),
+        Song (id: 3, name: "test3", artist: "test", capa: "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2019/03/c96c7a2-thank-u-next-ariana-album.jpg")]
+    
+    var body: some View {
+        NavigationStack{
+            ZStack{ // Gradiente de fundo
+                Rectangle()
+                    .ignoresSafeArea()
+                LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                
+                VStack { // Capa
+                    ScrollView{ //tela ScrollView
+                        
+                        Image("capa")
+                            .resizable()
+                            .frame(width: 250, height: 230)
+                        
+                        Text("Corosso")
+                            .foregroundColor(.white)
+                            .font(.system(size: 35))
+                            .offset(x: -100, y: 15)
+                        
+                        HStack{ // dono do album
+                            Image("truck")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                            
+                            Text("Alguém me ajuda")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                            Spacer()
+                        }.offset(x: 10)
+                        
+                        VStack{ //lista de musicas
+                            ForEach(songs) { song in
+                                NavigationLink(destination: MusicView(song: song)){ //link
+                                    HStack{
+                                        AsyncImage(url: URL(string: song.capa)) {
+                                            img_song in
+                                            img_song.resizable().frame(width: 60, height: 60)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        
+                                        VStack{
+                                            Text(song.name).foregroundColor(.white).font(.system(size: 20))
+                                            
+                                            Text(song.artist).foregroundColor(.white).font(.system(size: 20))
+                                        }
+                                        Text("                                             ")
+                                        Image(systemName: "ellipsis")
+                                            .foregroundStyle(.white)
+                                        Spacer()
+                                    } //HStack
+                                }//NavigationLink
+                            } //ForEach
+                        } //VStack da lista de musicas
+                    } //ScrollView
+                } //VStack inicial
+                .padding()
+            } //ZStack inicial
+        } //NavigationStack
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
