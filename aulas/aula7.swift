91cd810bb2783c6de1afa21442d202bd479877dd
Otavio Campos <70704777+ocamposbio@users.noreply.github.com>
2023-08-04 15:09:28 -0300

    Create aula7.swift

---
+++ aulas/aula7.swift
@@ -0,0 +1,55 @@
+/**
+
+Conceitos:
+Autenticaçao
+JSON
+Endpoint
+Objetos
+
+
+Endpoint: URL que entrega um json
+  decode e encode dos dados.
+
+Conceitos:
+
+Codable
+Optional
+Objetos
+Observable Object
+Published --> Array pode ser consumido fora da classe
+State Object -> State em objeto, quando um obj for modificado, eu recebo um alerta
+
+
+
+**/
+
+//Published example:
+class ViewModel : ObservableObject {
+@Published var personas : [Pessoas] = []
+
+}
+
+import foundation
+
+//Todos os nomes devem estar no json
+
+"Wand.core!" //--> Desempacotar o opcional
+
+struct Wand : Codable {
+  let wood : String? //campos opcionais: posso ou não receber um valor
+  let core : String?
+  let length : Double?
+}
+
+
+struct dados {
+  var nome: String
+  var idade: String
+}
+
+// Na conversão para JSON, ficaria assim:
+
+"dado": {
+  "nome": ""
+  "idade": ""
+}
