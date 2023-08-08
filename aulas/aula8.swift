cbb62eaf5ec7e7a43ee57bf0031e13c1cfa0a553
Otavio Campos <70704777+ocamposbio@users.noreply.github.com>
2023-08-08 16:26:05 -0300

    Create aula8.swift

---
+++ aulas/aula8.swift
@@ -0,0 +1,9 @@
+// Node-RED
+// Ferramenta de desenvolvimento baseada em fluxo de programação visual para conectar hardware, APIs e serviços online como IOT
+// --> CRUD
+// sudo npm install -g --unsafe-perm node-red
+// 3 tipos de nós básicos: Input, function, output
+// node-red versao 16
+// 
+// 
+// 
